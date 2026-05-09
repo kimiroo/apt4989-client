6f5076706e209c7752dd35a9c2188859d6c81cde
kimiroo <43537287+kimiroo@users.noreply.github.com>
2026-05-10 00:17:27 +0900

    Disable dynamic color Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "cc.darak.aptanywhere"
         minSdk = 33
         targetSdk = 36
-        versionCode = 30
-        versionName = "2.4.1"
+        versionCode = 32
+        versionName = "2.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
