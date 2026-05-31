5c9d28eb0c43e7316e41f4e1665f64b60e70c908
kimiroo <43537287+kimiroo@users.noreply.github.com>
2026-06-01 00:02:05 +0900

    Bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "cc.darak.aptanywhere"
         minSdk = 33
         targetSdk = 36
-        versionCode = 33
-        versionName = "2.4.3"
+        versionCode = 40
+        versionName = "2.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
